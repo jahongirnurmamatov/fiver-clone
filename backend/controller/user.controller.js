1102a9fda6ff8432b26a9ee6d1c67ea76e9ae8f4
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-28 11:57:04 +0500

    auth conller apis ready to be connected to client

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,3 +1,4 @@
+import { errorHandler } from "../middleware/errorHandler.js";
 import User from "../models/user.model.js";
 
 
@@ -6,10 +7,10 @@ export const deleteUser = async (req, res) => {
         const userId = req.userId;
         const user = await User.findById(req.params.id);
         if (!user) return res.status(404).send('User not found');
-        if (userId != req.params.id) return res.status(401).send('You can only delete your own account');
+        if (userId != req.params.id) return next(errorHandler(401,"You can delete only your account!"))
         await User.findByIdAndDelete(req.params.id);
         res.status(200).send('User has been deleted');
     } catch (error) {
-        res.status(500).send('An error occurred');
+        next(error)
     }
 };
