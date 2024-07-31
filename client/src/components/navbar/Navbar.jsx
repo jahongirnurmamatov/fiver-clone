d2dfdb7dd8be71b9c66b24657b4906e2fa50dcfc
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-31 22:18:19 +0500

    navbar ui options added

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,7 +3,12 @@ import './Navbar.scss'
 import {Link} from 'react-router-dom'
 const Navbar = () => {
     const [active,setActive]=useState(false);
-
+    const [open,setOpen] = useState(false);
+    const currentUser ={
+        id:1,
+        username:"John Done",
+        isSeller:true,
+    }
     const isActive = ()=>{
         window.scrollY > 0 ? setActive(true):setActive(false);
     }
@@ -28,8 +33,29 @@ const Navbar = () => {
                     <span>Explore</span>
                     <span>English</span>
                     <span>Sign in</span>
-                    <span>Become a Seller</span>
-                    <button>Join</button>
+                    {!currentUser?.isSeller&& <span>Become a Seller</span>}
+                    {!currentUser && <button>Join</button>}
+                    {currentUser &&(
+                        <div onClick={()=>setOpen(!open)} className='user'>
+                            <img src="https://www.perfocal.com/blog/content/images/size/w960/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg" alt="" />
+                            <span>{currentUser?.username}</span>
+                            {
+                                open&&<div className="options">
+                                {
+                                    currentUser?.isSeller &&(
+                                        <>
+                                        <span>Gigs</span>
+                                        <span>Add New Gig</span>
+                                        </>
+                                    )
+                                }
+                                <span>Orders</span>
+                                <span>Messages</span>
+                                <span>Logout</span>
+                            </div>
+                            }
+                        </div>
+                    )}
                 </div>
                 
             </div>
