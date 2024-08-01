3fc8a14afa7320f187f461f93a349c7196fa71e0
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-01 14:51:31 +0500

    Slider added using infinite-react-carousel library

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import Featured from '../../components/featured/Featured'
+import Slide from '../../components/slide/Slide'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
 import './Home.scss'
+import {cards} from '../../data'
+import CatCard from '../../components/catCard/CatCard'
 
 const Home = () => {
   return (
     <div>
       <Featured/>
       <TrustedBy/>
+      <Slide slidesToShow={5} arrowsScroll={3} >
+        {cards.map(card=>(
+          <CatCard key={card.id} item={card} arr/>
+        ))}
+      </Slide>
     </div>
   )
 }
