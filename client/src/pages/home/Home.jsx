4f8db33aefde31d3c15400639b40115f98b1fa11
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-01 16:25:00 +0500

    features componentd added to home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ import CatCard from '../../components/catCard/CatCard'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <Featured/>
       <TrustedBy/>
       <Slide slidesToShow={5} arrowsScroll={3} >
@@ -15,6 +15,50 @@ const Home = () => {
           <CatCard key={card.id} item={card} arr/>
         ))}
       </Slide>
+      <div className="features">
+        <div className="container">
+        <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your project within
+              minutes.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+            
+        
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
